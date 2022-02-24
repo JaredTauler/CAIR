da63d91a5d554150c169062b38e5dc07c57334e6
Student <j.tauler2021@jamesrumsey.net>
2022-02-24 11:33:38 -0500

    21

--- static/login/login.js
+++ static/login/login.js
@@ -1,4 +1,4 @@
-HeaderTitle("Attendance Centralized Interaction Reporting")
+HeaderTitle("Centralized Attendance Interaction Reporting")
 let username = document.querySelector("#username")
 let password = document.querySelector("#password")
 const FormElements = [username, password]
@@ -17,7 +17,7 @@ function CheckElement(element) {
     return false
 };
 
-document.getElementById("LoginButton").addEventListener("click", function(){
+function login() {
     let exit = false
     // Check each element before going ahead with the POST.
     FormElements.forEach(function (x, i)
@@ -29,34 +29,34 @@ document.getElementById("LoginButton").addEventListener("click", function(){
     )
     if (exit) {return false} // Break function if loop found elements guilty.
 
-    let formData = {
-        "username": $("#username").val(),
-        "password": $("#password").val(),
+    let formData = new FormData(document.getElementById("LoginForm"))
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            response = JSON.parse(xhttp.response)
+            if (this.status == 200) {
+                if (response[0] === "bad") {
+                    PopupText("Wrong " + response[1] + ".")
+                    ShowPopup()
+                }
+                if (response[0] === "redirect") {
+                    let cookie = JSON.parse(document.cookie)
+                    cookie["logged_in"] = true
+                    document.cookie = JSON.stringify(cookie)
+                    window.location.href = response[1]
+
+
+                }
+            }
+            else if (this.status == 500) {
+                PopupError(response)
+                ShowPopup()
+            }
+        }
     };
 
-    $.ajax({
-        type: "POST",
-        url: window.location.href,
-        dataType: "json",
-        encode: true,
-        data: formData,
-    // On response data:
-    }).done(function (data) {
-        if (data[0] === "bad") {
-            PopupText("Wrong " + data[1] + ".")
-            ShowPopup()
-        }
-        if (data[0] === "redirect") {
-            let cookie = JSON.parse(document.cookie)
-            cookie["logged_in"] = true
-            document.cookie = JSON.stringify(cookie)
-            window.location.href = data[1]
+    xhttp.open("POST", window.location.href, true);
+    xhttp.send(formData);
 
-
-        }
-
-    });
-
-    event.preventDefault();
     return false;
-});
+};
